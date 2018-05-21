23a673fa8696fddb1711da98e87350bdd70551a9
Stephen Smith <steve@newmexico.com>
2018-05-21 16:34:32 -0600

    lota changes

--- src/edu/cnm/deepdive/Summation.java
+++ src/edu/cnm/deepdive/Summation.java
@@ -1,19 +1,36 @@
 package edu.cnm.deepdive;
+/**
+ * Class for entry point to Calc.
+ * 
+ * @author Stephen
+ * @version 0.1
+ */
 
 public class Summation {
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    
+  private static int val;
 
+  public static void main(String[] args) {
+    
+    /**
+     * Command line entry point for RPN
+     * @param args operands and operators.
+     */
+    
     int sum=0;
-    for (int i = 0; i < args.length; i++ ) {
-      int val=Integer.parseInt(args[i]);
-      sum = sum + val;
+//    for (int i = 0; i < args.length; i++ ) 
+//      int val=Integer.parseInt(args[i]);
+//      sum = sum + val;
    
 
-  
+    int i = 0;
+    while (i<args.length) {
+      args[i] = args[1].trim();
+      int val = Integer.parseInt(args[i]);
+      sum += val;
+      i++;
   }
+    
    System.out.println(sum); 
     
    
