86cc0e2c1d654474274c79fb7b7bd97cc7c7fb0e
Stephen Smith <steve@newmexico.com>
2018-05-22 11:58:01 -0600

    added exp and synonyms

--- src/edu/cnm/deepdive/Summation.java
+++ src/edu/cnm/deepdive/Summation.java
@@ -14,21 +14,57 @@ public class Summation {
   public static void main(String[] args) {
     Stack<Double> stack = new Stack<>();
     for (int i = 0; i < args.length; i++) {
-      args[i] = args[i].trim();
-      if (args[i].equals("+")) {
-        double val1 = stack.pop();
-        double val2 = stack.pop();
-        stack.push(val1 + val2);
-      } else if (args[i].equals("-")) {
-        double val1 = stack.pop();
-        double val2 = stack.pop();
-        stack.push(val1 - val2);
-      } else {
-        double val = Double.parseDouble(args[i]);
-        stack.push(val);
+      String arg = args[i];
+      arg = arg.trim().toLowerCase();
 
+      /*
+       * if (arg.equals("+")) { double val1 = stack.pop(); double val2 = stack.pop();
+       * stack.push(val1 + val2); } else if (arg.equals("-")) { double val1 = stack.pop(); double
+       * val2 = stack.pop(); stack.push(val1 - val2); } else if (arg.equals("*")) { double val1 =
+       * stack.pop(); double val2 = stack.pop(); stack.push(val1 * val2); } else if
+       * (arg.equals("/")) { double val1 = stack.pop(); double val2 = stack.pop(); stack.push(val1 /
+       * val2); } else { double val = Double.parseDouble(arg); stack.push(val); }
+       */
+
+      double val1;
+      double val2;
+      switch (arg) {
+        // FIXME - duplicate statements with method call.
+        case "add":
+        case "+":
+          val1 = stack.pop();
+          val2 = stack.pop();
+          stack.push(val1 + val2);
+          break;
+        case "sub":
+        case "-":
+          val1 = stack.pop();
+          val2 = stack.pop();
+          stack.push(val1 - val2);
+          break;
+        case "mul":
+        case "*":
+          val1 = stack.pop();
+          val2 = stack.pop();
+          stack.push(val1 * val2);
+          break;
+        case "div":
+        case "/":
+          val1 = stack.pop();
+          val2 = stack.pop();
+          stack.push(val1 / val2);
+          break;
+        case "exp":
+        case "^":
+          val1 = stack.pop();
+          val2 = stack.pop();
+          stack.push(Math.pow(val1, val2));
+          break;
+        default:
+          val1 = Double.parseDouble(arg);
+          stack.push(val1);
+          break;
       }
-
     }
 
     System.out.printf("Result = %,.4f", stack.pop());
